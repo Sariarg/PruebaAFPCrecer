1edabbdeeb7cd3e14db6eae490283b5a8e7616a3
Sarai Argueta <emsarai15@gmail.com>
2021-09-09 11:12:55 -0600

    Api persona

---
+++ APIClinicasa/Controllers/PersonaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APIClinicasa.Resource;
+using APIClinicasa.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace APIClinicasa.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Nuevo(Persona modelo)
+        {
+            Conexion con = new Conexion();
+            con.Conectaar();
+            SqlCommand com = new SqlCommand(); // Create a object of SqlCommand class
+            com.Connection = con.con; //Pass the connection object to Command
+            com.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+            com.CommandText = "ControlPersonas"; //Stored Procedure Name
+
+            com.Parameters.Add("@i_metodo", SqlDbType.Int).Value = 1;
+            com.Parameters.Add("@i_nombre", SqlDbType.NVarChar).Value = modelo.nombre;
+            com.Parameters.Add("@i_Telefono", SqlDbType.Int).Value = modelo.telefono;
+            com.Parameters.Add("@i_dui", SqlDbType.NVarChar).Value = modelo.dui;
+            com.Parameters.Add("@i_nit", SqlDbType.NVarChar).Value = modelo.nit;
+            com.Parameters.Add("@i_fechaNacimiento", SqlDbType.Date).Value = Convert.ToDateTime(modelo.fechaNacimiento);
+
+            com.ExecuteNonQuery();
+
+            con.Desconectar();
+            return Ok("Persona Agregada!");
+        }
+    }
+}
---
+++ APIClinicasa/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIClinicasa.Models
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public int telefono { get; set; }
+        public string dui { get; set; }
+        public string nit { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+    }
+}
