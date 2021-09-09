199f37bac90d8872c4ed6360e3337a02260a4ff6
Sarai Argueta <emsarai15@gmail.com>
2021-09-09 11:22:28 -0600

    Api Doctor

---
+++ APIClinicasa/Controllers/DoctorController.cs
@@ -0,0 +1,37 @@
+﻿using APIClinicasa.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APIClinicasa.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace APIClinicasa.Controllers
+{
+    public class DoctorController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult NuevoDoc(Doctor modelo)
+        {
+            Conexion con = new Conexion();
+            con.Conectaar();
+            SqlCommand com = new SqlCommand(); // Create a object of SqlCommand class
+            com.Connection = con.con; //Pass the connection object to Command
+            com.CommandType = CommandType.StoredProcedure; // We will use stored procedure.
+            com.CommandText = "ControlDoctor"; //Stored Procedure Name
+
+            com.Parameters.Add("@i_metodo", SqlDbType.Int).Value = 1;
+            com.Parameters.Add("@i_JVM", SqlDbType.NVarChar).Value = modelo.JVM;
+            com.Parameters.Add("@i_idpersona", SqlDbType.Int).Value = modelo.idPersona;
+            com.Parameters.Add("@i_idespecialidad", SqlDbType.NVarChar).Value = modelo.idespecialidad;
+            
+            com.ExecuteNonQuery();
+
+            con.Desconectar();
+            return Ok("Doctor Agregado!");
+        }
+    }
+}
---
+++ APIClinicasa/Models/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIClinicasa.Models
+{
+    public class Doctor
+    {
+        public string JVM { get; set; }
+        public int idPersona { get; set; }
+        public string idespecialidad { get; set; }
+    }
+}
