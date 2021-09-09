5f82e524321a46a72825ca1e1cd93b5967a72986
Sarai Argueta <emsarai15@gmail.com>
2021-09-09 10:57:35 -0600

    Conexion

---
+++ APIClinicasa/Resource/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace APIClinicasa.Resource
+{
+    public class Conexion
+    {
+        public SqlConnection con = new SqlConnection("Data Source=DESKTOP-TQ5UTAN\\SQLEXPRESS;Initial Catalog=ClinicaSA;Persist Security Info=True;User ID=sa;Password=123456-");
+
+        public void Conectaar()
+        {
+            con.Open();
+        }
+
+
+        public void Desconectar()
+        {
+            con.Close();
+        }
+    }
+}
